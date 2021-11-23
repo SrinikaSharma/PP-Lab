74276bc7c3252c79c26657c3ffa893d642023c53
SrinikaSharma <93469490+SrinikaSharma@users.noreply.github.com>
2021-11-23 21:52:52 +0530

    Working with tuples

---
+++ Cycle 1 Lab Programs/Tuples.py
@@ -0,0 +1,24 @@
+cars = ("honda", "mercedes", "kia")
+print(cars)
+print(cars[1])
+
+cars = ("honda", "mercedes", "kia")
+for c in cars :
+    print(c) 
+
+cars = ("honda", "mercedes", "kia")
+if "mercedes" in cars :
+    print("Yes, 'mercedes' is found")
+
+print(len(cars))
+
+'''Output
+
+('honda', 'mercedes', 'kia')
+mercedes
+honda
+mercedes
+kia
+Yes, 'mercedes' is found
+3
+'''
