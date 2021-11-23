82e4fdd187c7a9a21d742c30357296bec2283185
SrinikaSharma <93469490+SrinikaSharma@users.noreply.github.com>
2021-11-23 21:36:26 +0530

    String Operations

---
+++ Cycle 1 Lab Programs/Strings.py
@@ -0,0 +1,33 @@
+my_string = 'Python'
+print(my_string)
+my_string = "Python"
+print(my_string)
+my_string = '''Python'''
+print(my_string)
+my_string = """Python"""
+print(my_string)
+
+str1 = 'BVRIT'
+str2 ='HYDERABAD'
+
+# using +
+print('str1 + str2 = ', str1 + str2)
+# using *
+print('str1 * 3 =', str1 * 3)
+
+s = 'Artificial Intelligence'
+print(s[0])
+print(s[:3])
+print(s[2:5])
+
+
+'''Output
+Python
+Python
+Python
+Python
+str1 + str2 =  BVRITHYDERABAD
+str1 * 3 = BVRITBVRITBVRIT
+A
+Art
+tif '''
