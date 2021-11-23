9c76b155fbf638571b2b14e4343817e9c1e44119
SrinikaSharma <93469490+SrinikaSharma@users.noreply.github.com>
2021-11-23 21:33:29 +0530

    DataTypes

---
+++ Cycle 1 Lab Programs/DataTypes.py
@@ -0,0 +1,11 @@
+num = 2003
+print(num, "is of type", type(num))
+num = 2003.5
+print(num, "is of type", type(num))
+num = 2+3j
+print(num, "is complex number?", isinstance(2+3j,complex))
+
+'''Output
+2003 is of type <class 'int'>
+2003.5 is of type <class 'float'>
+(2+3j) is complex number? True '''
