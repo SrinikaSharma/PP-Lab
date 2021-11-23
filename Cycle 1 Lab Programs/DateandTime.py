0cc8557ab35f9c85c0b0cc675247fa9492daa7c9
SrinikaSharma <93469490+SrinikaSharma@users.noreply.github.com>
2021-11-23 21:37:43 +0530

    Date and Time

---
+++ Cycle 1 Lab Programs/DateandTime.py
@@ -0,0 +1,8 @@
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+'''Output
+Current date and time : 
+2021-11-23 21:27:28 '''
